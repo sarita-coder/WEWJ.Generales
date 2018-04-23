4dbda25c00e63ed2489e59f0b64739ac32e0fe8a
Sarita Román <sarita.roman.colonio@gmail.com>
2018-04-23 09:51:10 -0500

    Actualizando 23-04-2015

--- js/main.js
+++ js/main.js
@@ -25,3 +25,20 @@ function handleComplete(evt) {
 	createjs.Ticker.setFPS(lib.properties.fps);
 	createjs.Ticker.addEventListener("tick", stage);
 }
+
+$('#exampleModalCenter').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
+
+$(function() {
+    $('.navbar-right li').click(function() {
+        $('.navbar-right li.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
